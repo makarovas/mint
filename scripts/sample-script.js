9ccd4c4109080f5164957afb0cf16fe346d22894
makarovas <makarofalex@gmail.com>
2022-03-17 18:45:31 +0200

    init

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -1,29 +1,14 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const Mint = await hre.ethers.getContractFactory("Mint");
+  const mint = await Mint.deploy("Minter", "MINT");
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await mint.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Mint deployed to:", mint.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import "./App.css";
+import mintApi from "./mintApi.json";
+import { ethers, BigNumber } from "ethers";
+import { useEffect, useState } from "react";
 
+const mintAddress = "0x5FbDB2sample-scri315678afecb367f032d93F642f64180aa3";
 function App() {
-  return <div className="App">Ping</div>;
+  // connecting
+  const [accounts, setAccounts] = useState([]);
+  console.log(accounts);
+
+  async function connectAccounts() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccounts(accounts);
+    }
+  }
+
+  useEffect(() => {
+    connectAccounts();
+  }, []);
+
+  // Minting
+  const [mintAmount, setMintAmount] = useState(1);
+
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(mintAddress, mintApi.abi, signer);
+      try {
+        const response = await contract.mint(BigNumber.from(mintAmount));
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  return (
+    <div className="App">
+      {accounts.length && (
+        <div>
+          <button onClick={() => setMintAmount(mintAmount - 1)}>-</button>
+          {mintAmount}
+          <button onClick={() => setMintAmount(mintAmount + 1)}>+</button>
+          <button onClick={handleMint}>Mint</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
